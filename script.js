c4959f4aae075db61937b1e49318680594301a41
THProff9 <tim09hendricks@gmail.com>
2023-09-23 13:25:47 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+const Frosch = document.getElementById("Frosch");
+const Bier = document.getElementById("Bier");
+
+function jump() {
+    if (Frosch.classList != "jump"){
+    Frosch.classList.add("jump");
+
+    setTimeout (function () {
+        Frosch.classList.remove("jump");
+    }, 350)
+    }
+    }
+
+    let isAlife = setInterval (function () {
+
+        let FroschTop = parseInt(window.getComputedStyle(Frosch).getPropertyValue("top"));
+
+        let BierLeft = parseInt(window.getComputedStyle(Bier).getPropertyValue("left"));
+        
+
+        if (BierLeft <90 && BierLeft > 0 && FroschTop >= 210){
+
+            alert("Spiel Über du Angelsachse");
+        }
+    }, 10);
+
+document.addEventListener("keydown", function (event) {
+    jump();
+});
